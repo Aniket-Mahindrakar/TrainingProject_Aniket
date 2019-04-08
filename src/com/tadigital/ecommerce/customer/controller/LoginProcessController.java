9d4ecbc2a764eaa64daeec58e9d13ffc914d7c77
Aniket Mahindrakar <aniketmm98@gmail.com>
2019-04-08 16:06:51 +0530

    Modifieed Login Process functionality

--- src/com/tadigital/ecommerce/customer/controller/LoginProcessController.java
+++ src/com/tadigital/ecommerce/customer/controller/LoginProcessController.java
@@ -16,6 +16,21 @@ import com.tadigital.ecommerce.customer.service.CustomerService;
 @WebServlet("/login")
 public class LoginProcessController extends HttpServlet {
 	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		HttpSession sess = req.getSession();
+		String name = (String) sess.getAttribute("NAME");
+		if (name != null) {
+			RequestDispatcher rd = req.getRequestDispatcher("CustomerAccount.jsp");
+			rd.forward(req, resp);
+		} else {
+			RequestDispatcher rd = req.getRequestDispatcher("HomePage.jsp");
+			rd.forward(req, resp);
+		}
+		
+
+	}
+
+	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		String email = req.getParameter("log_in_email");
 		String pass = req.getParameter("log_in_pwd");
@@ -24,20 +39,27 @@ public class LoginProcessController extends HttpServlet {
 		cust.setEmail(email);
 		cust.setPass(pass);
 
-		CustomerService cust_ser = new CustomerService();
-		boolean status = cust_ser.loginCustomer(cust);
-		if (status) {
-			HttpSession ses = req.getSession();
-			ses.setAttribute("CUSTOMERDATA", cust);
-			req.setAttribute("log", "Y");
+		CustomerService custSer = new CustomerService();
+		boolean status = custSer.loginCustomer(cust);
+		try {
+			if (status) {
+				HttpSession ses = req.getSession();
+				ses.setAttribute("NAME", cust.getFname() + " " + cust.getLname());
+				ses.setAttribute("CUSTOMERDATA", cust);
+				req.setAttribute("log", "Y");
 
-			RequestDispatcher rd = req.getRequestDispatcher("CustomerAccount.jsp");
-			rd.forward(req, resp);
+				RequestDispatcher rd = req.getRequestDispatcher("CustomerAccount.jsp");
+				rd.forward(req, resp);
 
-		} else {
-			req.setAttribute("log", "N");
-			RequestDispatcher rd = req.getRequestDispatcher("SignInSignUpForms.jsp");
-			rd.forward(req, resp);
+			} else {
+				req.setAttribute("log", "N");
+				RequestDispatcher rd = req.getRequestDispatcher("SignInSignUpForms.jsp");
+				rd.forward(req, resp);
+			}
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
+		} catch (ServletException se) {
+			se.printStackTrace();
 		}
 	}
 }
